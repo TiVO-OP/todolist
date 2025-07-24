784598faff38b3b845fda6ba6ac6d29f177ece8a
TiVO-OP <81425833+TiVO-OP@users.noreply.github.com>
2025-07-24 17:37:04 +0200

    monitor V1.0.1

--- monitor.lua
+++ monitor.lua
@@ -1,20 +1,21 @@
-
 modem = peripheral.find("modem", rednet.open)
-monitor = peripheral.find("monitor")
 rednet.CHANNEL_BROADCAST = 55859
-monitor.setPaletteColor(colors.magenta,0xa848e8)
+term.setPaletteColor(colors.magenta,0xa848e8)
 local wrap = require "cc.strings".wrap
+local inputting = ""
 function start()
-    monitor.setCursorPos(2,1)
-    monitor.setBackgroundColor(colors.purple)
-    monitor.setTextColor(colors.white)
-    monitor.write("TO-DO LIST:")
+    term.setCursorPos(2,1)
+    term.setBackgroundColor(colors.purple)
+    term.setTextColor(colors.white)
+    term.write("TO-DO LIST:")
     return 0
 end
-local monitorW,monitorH = monitor.getSize()
+local del_pressed=0
 local deletenumber=0
 local line=2
+local writing = false
 local deleting=false
+local adding=false
 local lineindex={}
 local tasks = {}
 for line in io.lines("tasks.txt") do
@@ -36,99 +37,236 @@ end
 
 function writetasks()
     lineindex={}
-        line=2
+    line=2
+        adding=false
         for k,v in pairs(tasks) do
             table.insert(lineindex,line,k)
-            monitor.setBackgroundColor(colors.magenta)
-            monitor.setCursorPos(2,line)
-            monitor.clearLine()
+            term.setBackgroundColor(colors.magenta)
+            term.setCursorPos(2,line)
+            term.clearLine()
+            term.setBackgroundColor(colors.purple)
+            term.setCursorPos(1,line)
+            term.write(" ")
             if v.isDone==0 then
-                monitor.setBackgroundColor(colors.white)
+                bg=(colors.white)
+                fg=(colors.black)
             else 
-                monitor.setBackgroundColor(colors.lime)
+                bg=(colors.lime)
+                fg=(colors.white)
             end
-            monitor.write("\x88")
-            monitor.setBackgroundColor(colors.red)
-            monitor.setCursorPos(monitorW,line)
-            monitor.write("X")
-            for i,wers in pairs(wrap(v.tasken,monitorW-4)) do
+            term.setTextColor(fg)
+            term.setBackgroundColor(bg)
+            term.write("\x88")
+            term.setBackgroundColor(colors.magenta)
+            term.setTextColor(bg)
+            term.write("\x95")
+            term.setBackgroundColor(colors.red)
+            term.setCursorPos(26,line)
+            term.setTextColor(colors.white)
+            term.write("X")
+            for i,wers in pairs(wrap(v.tasken,21)) do
                 if i>1 then
-                    monitor.setBackgroundColor(colors.magenta)
-                    monitor.setCursorPos(3,line)
-                    monitor.write(string.rep(" ",monitorW-1))
-                    monitor.setCursorPos(4,line)
-                    monitor.write(wers)
+                    term.setBackgroundColor(colors.magenta)
+                    term.setCursorPos(4,line)
+                    term.write(string.rep(" ",24))
+                    term.setCursorPos(4,line)
+                    term.setTextColor(bg)
+                    term.write(wers)
                     line=line+1
                 else
-                    monitor.setBackgroundColor(colors.magenta)
-                    monitor.setCursorPos(4,line)
-                    monitor.write(wers)
+                    term.setBackgroundColor(colors.magenta)
+                    term.setCursorPos(4,line)
+                    term.setTextColor(bg)
+                    term.write(wers)
                     line=line+1
                 end
             end
         end
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+    term.setCursorPos(2,line+2)
+    term.write(" add ")
     return 0
 end
 
+function addtask()
+    adding=true
+    term.setCursorPos(3,5)
+    for i=1,5,1 do
+        term.setBackgroundColor(colors.lightGray)
+        term.write(string.rep(" ",22))
+        term.setCursorPos(3,i+5)
+    end
+    term.setCursorPos(4,5)
+    term.setTextColor(colors.black)
+    term.write("Input task name:")
+    term.setCursorPos(4,7)
+    term.setBackgroundColor(colors.white)
+    term.write(string.rep(" ", 20))
+    term.setCursorPos(4,9)
+    term.setBackgroundColor(colors.red)
+    term.write("Cancel")
+    term.setCursorPos(17,9)
+    term.setBackgroundColor(colors.lime)
+    term.write("Confirm")
+    return 0
+end
 function deletetask(tasknumber)
     deleting=true
     deletenumber=tasknumber
-    monitor.setCursorPos(3,5)
+    term.setCursorPos(3,5)
     for i=1,5,1 do
-        monitor.setBackgroundColor(colors.lightGray)
-        monitor.write(string.rep(" ",22))
-        monitor.setCursorPos(3,i+5)
+        term.setBackgroundColor(colors.lightGray)
+        term.write(string.rep(" ",22))
+        term.setCursorPos(3,i+5)
     end
-    monitor.setCursorPos(6,6)
-    monitor.setTextColor(colors.black)
-    monitor.write("Delete task #"..tasknumber.."?")
-    monitor.setCursorPos(4,9)
-    monitor.setBackgroundColor(colors.red)
-    monitor.write("Cancel")
-    monitor.setCursorPos(17,9)
-    monitor.setBackgroundColor(colors.lime)
-    monitor.write("Confirm")
+    term.setCursorPos(6,6)
+    term.setTextColor(colors.black)
+    term.write("Delete task #"..tasknumber.."?")
+    term.setCursorPos(4,9)
+    term.setBackgroundColor(colors.red)
+    term.write("Cancel")
+    term.setCursorPos(17,9)
+    term.setBackgroundColor(colors.lime)
+    term.write("Confirm")
     return 0
 end
-
-monitor.setBackgroundColor(colors.purple)
-monitor.clear()
+function draw()
+    cursorx,cursory=term.getCursorPos()
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+    term.setCursorPos(4,7)
+    term.write(string.rep(" ",20))
+    term.setCursorPos(4,7)
+    term.write(inputting)
+    if cursorx~=4+#inputting and del_pressed == 0 then
+        term.setCursorPos(cursorx+1,7)
+    elseif cursorx~=4+#inputting and del_pressed == 1 then
+        term.setCursorPos(cursorx-1,7)
+        del_pressed=0
+    end
+    return 0
+end
+term.setBackgroundColor(colors.purple)
+term.clear()
 start()
 writetasks()
 while true do
     local event,button,x,y = os.pullEvent("mouse_click")
     for p,c in pairs(lineindex) do
-        if button==1 and y==p and x==26 then
+        if adding==false and button==1 and y==p and x==26 then
             deletetask(c)
-        end
-        if deleting==false and button==1 and y==p and x==1 then
+        elseif adding==false and deleting==false and button==1 and y==p and x>=2 and x<=25 then
             if tasks[c].isDone == 0 then
                 tasks[c].isDone = 1
             else 
                 tasks[c].isDone = 0
             end
             savetofile()
-            monitor.setBackgroundColor(colors.purple)
-            monitor.clear()
+            term.setBackgroundColor(colors.purple)
+            term.clear()
             start()
             writetasks()
         end
     end
-    if button==1 and deleting==true and x<=10 and x>=4 and y==9 then
+    if deleting==false and adding==false and button==1 and x>=2 and x<=6 and y==line+2 then
+        addtask()
+        term.setCursorPos(4+#inputting,7)
+        term.setCursorBlink(true)
+        writing=true
+    elseif button==1 and adding==false and deleting==true and x<=10 and x>=4 and y==9 then
         deleting=false
-        monitor.setBackgroundColor(colors.purple)
-        monitor.clear()
+        term.setBackgroundColor(colors.purple)
+        term.clear()
         start()
         writetasks()
-    end
-    if button==1 and deleting==true and x<=24 and x>=17 and y==9 then
+    elseif button==1 and adding==false and deleting==true and x<=24 and x>=17 and y==9 then
         deleting=false
         table.remove(tasks,deletenumber)
         savetofile()
-        monitor.setBackgroundColor(colors.purple)
-        monitor.clear()
+        term.setBackgroundColor(colors.purple)
+        term.clear()
         start()
         writetasks()
+    elseif button==1 and deleting==false and adding==true and x<=10 and x>=4 and y==9 then
+        term.setBackgroundColor(colors.purple)
+        term.clear()
+        start()
+        writetasks()
+        term.setCursorBlink(false)
+        inputting=""
+        adding=false
+        writing=false
+    elseif button==1 and deleting==false and adding==true and x>=17 and x<=24 and y==9 then
+        table.insert(tasks, {isDone=0, tasken=inputting})
+        savetofile()
+        inputting=""
+        term.setBackgroundColor(colors.purple)
+        term.clear()
+        start()
+        writetasks()
+        adding=false
+        writing=false
+    elseif button==1 and deleting==false and adding==true and x>=4 and x<=24 and y==7 then
+        term.setCursorPos(4+#inputting,7)
+        term.setCursorBlink(true)
+        writing=true
     end
-    os.sleep(0.5)
+    while writing==true do
+        local e1, p1, x, y = os.pullEvent()
+        if e1 == "key" then
+            if p1 == keys.backspace then
+                cursorx,cursory=term.getCursorPos()
+                local CharPosInput = cursorx-4
+                if CharPosInput>0 then
+                    del_pressed = 1
+                    inputting = inputting:sub(1, CharPosInput - 1) .. inputting:sub(CharPosInput + 1)
+                    cursorx = cursorx-1
+                end
+                draw()
+            elseif p1 == keys.left then
+                cursorx,cursory = term.getCursorPos()
+                if cursorx>=5 then
+                    term.setCursorPos(cursorx-1,cursory)
+                end
+            elseif p1 == keys.right then
+                cursorx,cursory = term.getCursorPos()
+                if cursorx<4+#inputting then
+                    term.setCursorPos(cursorx+1,cursory)
+                end
+            end
+        elseif e1 == "char" then
+            cursorx,cursory = term.getCursorPos()
+            inputting=string.sub(inputting,1,cursorx-4)..p1..string.sub(inputting,cursorx-3)
+            draw()
+        elseif e1=="mouse_click" then
+            if x>=5 and x<=24 and y==7 then
+
+            elseif x>=4 and x<=10 and y==9 then
+                term.setBackgroundColor(colors.purple)
+                term.clear()
+                start()
+                writetasks()
+                term.setCursorBlink(false)
+                inputting=""
+                adding=false
+                writing=false
+            elseif x>=17 and x<=24 and y==9 then
+                term.setCursorBlink(false)
+                table.insert(tasks, {isDone=0,tasken=inputting})
+                savetofile()
+                inputting=""
+                term.setBackgroundColor(colors.purple)
+                term.clear()
+                start()
+                writetasks()
+                adding=false
+                writing=false
+            else
+                writing=false
+                term.setCursorBlink(false)
+            end
+        end
+    end
+    os.sleep(0.1)
 end
