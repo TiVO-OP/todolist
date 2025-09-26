33aa92ed6728b0843b82ff8d87325520fc0b6b50
TiVO <81425833+TiVO-OP@users.noreply.github.com>
2025-09-26 20:19:31 +0200

    startup.lua

---
+++ startup.lua
@@ -0,0 +1,15 @@
+if fs.exists("config.json") then
+    local configFile = fs.open("config.json", "r")
+    local configFromFile = configFile.readAll()
+    configFile.close()
+
+    if configFromFile~="" then
+        if fs.exists("monitor.lua") then
+            shell.run("monitor.lua")
+        else 
+            shell.run("pocketpc.lua")
+        end
+    end
+else
+    shell.run("configure.lua")
+end
